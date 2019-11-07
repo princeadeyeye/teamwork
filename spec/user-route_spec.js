bf8b587f86239feaea58af34369da13ef8af1976
Muiz Adeyeye <muizadeyeye@hmail.com>
2019-11-07 04:09:19 +0100

    adding admin and user test

---
+++ spec/user-route_spec.js
@@ -0,0 +1,47 @@
+const request = require('request');
+const server = require('../backend/server')
+const base_url = 'http://localhost:5000/'
+
+
+describe("User Route", function() {
+
+
+  describe("Post User Route", function() {
+    it("returns status code 201", function(done) {
+      request.post(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(201);
+        done();
+      });
+    });
+
+  });
+
+describe("Update User Route", function() {
+    it("returns status code 200", function(done) {
+      request.patch(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+
+describe("Delete User Route", function() {
+    it("returns status code 204", function(done) {
+      request.delete(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(204);
+        done();
+      });
+    });
+  });
+
+describe("Get User Route", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+
+
+})
