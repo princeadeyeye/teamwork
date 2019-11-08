4dadbd423ca534f97b296a1c43d7d17002030ecd
Muiz Adeyeye <muizadeyeye@hmail.com>
2019-11-08 20:53:25 +0100

    fixing articles bugs

--- backend/controllers/article-controller.js
+++ backend/controllers/article-controller.js
@@ -62,9 +62,9 @@ async function updateArticle(req, res) {
 
 //to be comment out
 async function listArticles(req, res) {
-    const texts = 'SELECT * FROM articles ORDER BY articleid ASC';
+    const articlesQ = 'SELECT * FROM articles ORDER BY articleid ASC'; 
     try {
-      const { rows } = await pool.query(texts);
+      const { rows } = await pool.query(articlesQ);
       if (!rows) {
         return res.status(404).json({'message': 'articles not found'});
       }
@@ -74,14 +74,22 @@ async function listArticles(req, res) {
     }
   }
 async function getArticle(req, res) {
-    const text = 'SELECT * FROM articles WHERE articleid = $1';
+    const articleCommentQ = `SELECT articles.articleid, title, article, articles.createdOn, userid, 
+                              commentid, comment, a_comments.createdOn
+                            FROM articles
+                              INNER JOIN a_comments ON a_comments.articleid = articles.articleid 
+                            WHERE articles.articleid = $1`;
+          const articleQ = `SELECT * FROM articles WHERE articleid = $1`;
     try {
-      const { rows } = await pool.query(text, [req.params.id]);
+      const { rows } = await pool.query(articleCommentQ, [req.params.id]);
       if (!rows[0]) {
-        return res.status(404).json({'message': 'article not found'});
+          const response = await pool.query(articleQ, [req.params.id]);
+          if(!response.rows[0]){
+            return res.status(404).json({'message': 'article not found'});
+          }
+              return res.status(200).json(response.rows[0])
       }
       return res.status(200).json(rows[0]);
-
     } catch(error) {
       return res.status(400).json(error)
     }
@@ -95,33 +103,53 @@ async function getArticle(req, res) {
           if(!rows[0]) {
             return res.status(400).json({'message': 'Article not found'})
           }
-          return res.status(204).json({message: 'Article Successfully deleted'})
+          return res.status(204).send({'message': 'Article Successfully deleted'})
       } catch(error) {
       return res.status(404).json(error)
     }
   }
 
 async function commentArticle (req, res) {
-    const createCommentQuery = `
+      const insertCommentq = `
     INSERT INTO
       a_comments(
-        comment,
-        title, 
-        article,     
-        createdOn
+        comment,     
+        articleid,
+        createdOn      
         )
-      VALUES($1, $2, $3, $4)
+      VALUES($1, $2, $3)
       returning *`;
-    const values = [
+    let insertvalue = [
       req.body.comment,
-      req.body.title,
-      req.body.article,
+      req.body.articleid,
+      moment(new Date())
+    ];
+
+    const findOneQ = 'SELECT * FROM a_comments WHERE articleid=$1';
+    const updateOneQ =`UPDATE a_comments
+                        SET comment = $1, createdOn=$2 
+                        returning *`;
+    const commentArticleQ = `SELECT title, article, a_comments.createdOn, comment
+                            FROM articles
+                              INNER JOIN a_comments ON a_comments.articleid = articles.articleid 
+                            WHERE articles.articleid = $1`;
+    let values = [
+      req.body.comment,
       moment(new Date())
     ];
 
     try {
-      const { rows } = await pool.query(createCommentQuery, values);
-        return res.status(201).json(rows[0]);
+    const { rows } = await pool.query(insertCommentq, insertvalue);
+    const find = await pool.query(findOneQ, [req.params.id])
+      if(!find.rows[0]) {
+        res.status(400).json({message: "Article not found"})
+      }
+        const response = await pool.query(updateOneQ, values);
+        if(!response.rows[0]) {
+          res.status(400).json({message: "Unable to comment on article"})
+        }
+        const message = await pool.query(commentArticleQ, [req.params.id]);
+       return res.status(201).json(message.rows[0]);
     } catch(error) {
       return res.status(400).json(error);
     }
