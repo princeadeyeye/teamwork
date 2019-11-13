d40b2f52a1b0cf4e7b4061492e4b768d064a0b0e
Muiz Adeyeye <muizadeyeye@hmail.com>
2019-11-13 03:13:28 +0100

    adding test

--- backend/test/article-route.test.js
+++ backend/test/article-route.test.js
@@ -4,6 +4,27 @@ const app = require('../app.js')
 
 describe("Article Route", () => {
 
+    /*
+      declare the token variable in a scope accessible
+      by the entire test suite
+  
+    let token;
+
+    beforeAll((done) => {
+      request(app)
+        .post('/auth/v2/signin')
+        .send({
+          username: m2@gmail.com,
+          password: 123,
+        })
+        .end((err, response) => {
+          token = response.body.token; // save the token!
+          done();
+        });
+    });*/
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU3MzYwNTg1NSwiZXhwIjoxNTczNjkyMjU1fQ.htcjbwkmaK5DW4X9XaX-doqKcOZKFBQFirkIGiwemVk'
+
 
   describe("Post Article Route", () => {
     test("should not returns status code 201", (done) => {
@@ -17,7 +38,7 @@ describe("Article Route", () => {
 
 describe("Update Article Route", () => {
     test("it should returns status code 200", (done) => {
-        request(app).patch('/v2/articles/1').then((response) => {
+        request(app).patch('/api/v2/articles/1').then((response) => {
         expect(response.statusCode).not.toBe(200);
         done();
       });
@@ -26,7 +47,7 @@ describe("Update Article Route", () => {
 
 describe("Delete Article Route", () => {
     test("it should returns status code 204", (done) => {
-      request(app).delete('/v2/articles/1').then((response) => {
+      request(app).delete('/api/v2/articles/1').then((response) => {
         expect(response.statusCode).not.toBe(200);
         done();
       });
@@ -35,8 +56,12 @@ describe("Delete Article Route", () => {
 
 describe("Get Article Route", () => {
     test("it should returns status code 200", (done) => {
-      request(app).get('/v2/articles/1').then((response) => {
-        expect(response.statusCode).not.toBe(200);
+      request(app)
+      .get(`/api/v2/articles/1`)
+      .then((response) => {
+        expect(response.statusCode).toBe(401);
+        console.log(response.headers);
+        expect(response.unauthorized).toBe(true)
         done();
       });
     });
@@ -44,7 +69,7 @@ describe("Get Article Route", () => {
 
 describe("Get Article Route", () => {
     test("it should not  returns status code 200", (done) => {
-      request(app).get('/v2/articles/').then((response) => {
+      request(app).get('/api/v2/articles/').then((response) => {
         expect(response.statusCode).not.toBe(200);
         done();
       });
@@ -54,7 +79,7 @@ describe("Get Article Route", () => {
 
 describe("Put Article Comment ", () => {
     test("it should  not returns status code 200", (done) => {
-      request(app).put('/v2/articles/:id/comment').then((response) => {
+      request(app).put('/api/v2/articles/1/comment').then((response) => {
         expect(response.statusCode).not.toBe(201);
         done();
       });
--- backend/test/user-route.test.js
+++ backend/test/user-route.test.js
@@ -2,12 +2,21 @@ const request = require('supertest');
 const app = require('../app')
 
 
-describe("User Route", function() {
+describe("tesing unautheticated routes", function() {
 
 
-describe("Get User Route", () => {
+describe("Get Documentation", () => {
     test("it should returns status code 200", (done) => {
       request(app).get('/').then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+
+describe("Get all feeds routes", () => {
+    test("should get feeds including articles and gifs", (done) => {
+      request(app).get('/api/v3/feeds').then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
